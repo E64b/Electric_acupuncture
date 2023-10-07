bebb641c6fdd92cf4d0748fd232b936449d0459e
Mike <125614854+E64b@users.noreply.github.com>
2023-10-07 18:54:35 +0300

    inv

--- test_inv/test_inv.ino
+++ test_inv/test_inv.ino
@@ -1,34 +1,37 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <stdio.h>
-#include "LiquidCrystal_I2C.h"
+
 #include "GyverHC595.h"
+#include "LiquidCrystal_I2C.h"
 
-
-#define STCP 10 //pinCS 12
-#define DS 11 //pinDT 14
-#define SHCP 13 //pinCLK 11
+#define STCP 10  // pinCS 12
+#define DS 11    // pinDT 14
+#define SHCP 13  // pinCLK 11
 
 #define SHIFTS 1
 #define HC595_DELAY 0
 #define ONE_SHIFT_DATA 8
 #define ALL_DATA (SHIFTS * ONE_SHIFT_DATA)
 
-
 GyverHC595<SHIFTS, HC_PINS> reg (STCP, DS, SHCP);
 LiquidCrystal_I2C lcd (0x27, 16, 2);
 
-int16_t CURRENT_PIN = -1;
+uint16_t CURRENT_PIN = 0;
 uint32_t TIMER = 0;
 uint16_t OLD_PIN = 0;
-bool FIRST = true;
 bool lcdRedraw = false;
 
 void setup (){
 	/*Init Serial*/
 	Serial.begin (9600);
-	while (!Serial);
+	while (!Serial)
+		;
 	Serial.print ("Serial OK");
+
+	/*Init reg*/
+	reg.setAll ();
+	reg.update ();
 
 	/*Init LCD*/
 	lcd.init ();
@@ -40,27 +43,26 @@ void setup (){
 	lcd.print ("Electric acu");
 	delay (1000);
 	lcd.clear ();
-
-	/*Init reg*/
-	reg.clearAll ();	}
+	}
 
 void loop (){
-
-	/*We turn on the pins in turn in increments of 1 minute*/
-	if ((millis () - TIMER >= 10000) or (FIRST)){
+	/*We turn on the pins in turn in increments of time*/
+	if ((millis () - TIMER >= 1000) or (TIMER == 0)){
 		TIMER = millis ();
 		lcdRedraw = true;
 		OLD_PIN = CURRENT_PIN;
-		if (CURRENT_PIN <= ALL_DATA){
+		if (CURRENT_PIN < ALL_DATA){
 			CURRENT_PIN++;
-			reg.set (CURRENT_PIN);
-			Serial.println (CURRENT_PIN);
 			if (CURRENT_PIN >= ALL_DATA){
 				CURRENT_PIN = 0;
 				}
-			if (!FIRST){
+
+			reg.clear (CURRENT_PIN);
+			Serial.println (CURRENT_PIN);
+
+			if (TIMER != 0){
 				Serial.println (OLD_PIN);
-				reg.clear (OLD_PIN);
+				reg.set (OLD_PIN);
 				}
 			}
 		reg.update ();
@@ -68,9 +70,8 @@ void loop (){
 		}
 
 	/*If the value has changed, we change it on the screen*/
-	if ((OLD_PIN != CURRENT_PIN and lcdRedraw) or (FIRST)){
+	if (OLD_PIN != CURRENT_PIN and lcdRedraw){
 		lcdRedraw = false;
-		FIRST = false;
 		lcd.clear ();
 		OLD_PIN = CURRENT_PIN;
 		lcd.home ();
