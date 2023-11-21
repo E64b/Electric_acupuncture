ddab636897d32c6c1da0d4dfd6b563705d885a91
Mike <125614854+E64b@users.noreply.github.com>
2023-11-21 18:44:39 +0300

    Example

--- example/test/test.ino
+++ example/test/test.ino
@@ -1,18 +1,19 @@
 #include "GyverHC595.h"
 #include <Arduino.h>
 #include <SPI.h>
+#include "LiquidCrystal_I2C.h"
 
-// Поменяй пины под ESP
 #define STCP 10 // pinCS 12
 #define DS 11   // pinDT 14
 #define SHCP 13 // pinCLK 11
 
-#define SHIFTS 10
+#define SHIFTS 10 //Колличество чипов 595 подключенных последовательно
 #define HC595_DELAY 0
 #define ONE_SHIFT_DATA 8
 #define ALL_DATA (SHIFTS * ONE_SHIFT_DATA)
 
 GyverHC595<SHIFTS, HC_SPI> reg(STCP);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 uint16_t OLD_PIN = 0;
 
 void setup() {
@@ -20,8 +21,10 @@ void setup() {
   while (!Serial) {
   }
   Serial.print("Serial OK");
-
-  delay(1000);
+  lcd.begin();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.printstr("LCD OK");
   reg.clearAll();
   reg.update();
   delay(1000);
@@ -30,11 +33,14 @@ void setup() {
 void loop() {
   /*Включаем все по одному потом выключаем и опять */
   for (uint16_t CURRENT_PIN = 0; CURRENT_PIN < ALL_DATA; CURRENT_PIN++) {
-    reg.clear(OLD_PIN);
+    reg.write(OLD_PIN, false);
+    reg.write(CURRENT_PIN, true);
     reg.update();
-    reg.set(CURRENT_PIN);
-    reg.update();
-    delay(60000);
+    lcd.clear();
+    lcd.home();
+    lcd.printstr("PIN = ");
+    lcd.printstr(CURRENT_PIN);
+    delay(30000); //Тут время на выход в Миллисекундах
     OLD_PIN = CURRENT_PIN;
   }
   reg.clearAll();
