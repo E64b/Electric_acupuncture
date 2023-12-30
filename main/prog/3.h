433fa7562ede5307bdc0bda4ffc2130b4cf49e25
Mike <125614854+E64b@users.noreply.github.com>
2023-12-30 12:59:10 +0300

    edit

--- main/prog/3.h
+++ main/prog/3.h
@@ -3,4 +3,4 @@
 //Here it is necessary to enter the order of operation of the program, the numbers from 0 to 63.
 //Example: const uint8_t _3[64] PROGMEM = {6,12,35,0, 18, 22, 53,..n,..44};
 
-const uint8_t _3[64] PROGMEM = {};
+const uint16_t _3[64] PROGMEM = {};
--- main/work.cpp
+++ main/work.cpp
@@ -1,6 +1,15 @@
 #include "main.h"
 
+
+uint16_t getExit(const uint16_t *pChar, uint16_t index) { 
+return pChar[index];
+}
+
 void work() {
   if (data.work) {
+    const uint8_t *GetProg(data.program);
+    uint16_t exit = getExit(img, i);
+
+    data.send = true;
   }
 }
